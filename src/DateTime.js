81d38b0b9fb3ce8b4ed0b794c92e9b7e35be8a87
sowndu <sowndu@sowndarya>
2015-08-16 17:46:54 +0700

    commit changes

--- src/DateTime.js
+++ src/DateTime.js
@@ -1,5 +1,9 @@
 // Given a date in string format, return the day it corresponds to.
 exports.GetDay = function(dateString){
-
+	var d=new Date(dateString);
+   if(dateString===""||dateString==null||dateString==="invalid datestring"||d==="Invalid Date")
+	   return "hello";
+   else 
+	   return d.getDay();
 }
 
