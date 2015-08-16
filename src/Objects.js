6806e28c164f518e4b950b5d70c262d824ad8272
sowndu <sowndu@sowndarya>
2015-08-16 19:54:31 +0700

    commit changes

--- src/Objects.js
+++ src/Objects.js
@@ -1,8 +1,20 @@
 exports.GetObjectPropertyNames = function(obj) {
-
+	var x=new Array();
+ if(obj==null)
+	 return null;
+ for(prop in obj)
+	 x.push(prop);
+ return x;
 }
 
 exports.GetObjectPropertyValues = function(obj) {
-
+    var x=new Array();
+	if(obj===null)
+		return null;
+	for(val in obj)
+	{
+		x.push(obj[val]);
+	}
+	return x;
 }
 
