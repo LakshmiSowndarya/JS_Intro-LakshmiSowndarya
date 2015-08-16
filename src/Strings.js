b286e2f10b0e55fba7461c14a74b54a796d410d3
sowndu <sowndu@sowndarya>
2015-08-16 19:10:10 +0700

    commit changes

--- src/Strings.js
+++ src/Strings.js
@@ -1,5 +1,10 @@
 exports.ReverseString = function(str){
-
+    var i,temp="";
+	for(i=str.length-1;i>=0;i--)
+	{
+		temp=temp+str[i];
+	}
+	return temp;
 }
 
 // Given a string with lname,fname - return an array with the first names.
@@ -7,16 +12,60 @@ exports.ReverseString = function(str){
 // an array ["Bill","Steve","Larry"]
 
 exports.GetFirstNames = function(str){
+	var temp="",arr=new Array();
+	if(str!=null&&str!=";"&&str!="")
+	{
+	for(i=0;i<str.length;i++)
+	{
+		temp="";
+		while(str[i]!=','&&i<str.length)
+		{
+			i++;
+		}
+		i++;
+		while(str[i]!=';'&&i<str.length)
+		{
+			temp=temp+str[i];
+			i++;
+		}
+		arr.push(temp);
+	}
+    }
+	else if(str===null)
+		return null;
+	else if(str===";"||str==="")
+		arr=[];
+	return arr;
 }
 
 
 exports.ReverseArrayOfStrings = function(arrayOfStrings){
-
+    var i=0,j=0,temp="";
+	for(i=0;i<arrayOfStrings.length;i++)
+	{
+		temp="";
+		for(j=arrayOfStrings[i].length-1;j>=0;j--)
+	{
+		temp=temp+arrayOfStrings[i][j];
+	}
+	arrayOfStrings[i]=temp;
+	}
+	return arrayOfStrings;	
 }
 
 // Given an array of sentences, create a paragraph by concatenating all the strings and adding
 // fullstop (.) after each senetences.
 exports.CreateParagraph = function(arrayOfStrings){
-
+   var i,temp="";
+   if(arrayOfStrings===null)
+	  temp=null;
+   if(arrayOfStrings===[])
+	   temp=[];
+   if(arrayOfStrings!=null)
+   {
+   for(i=0;i<arrayOfStrings.length;i++)
+	   temp=temp+arrayOfStrings[i]+'.';
+   }
+   return temp;
 }
 
